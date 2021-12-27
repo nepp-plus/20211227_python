fe8afc72e1f8f95516ce3735d9a2b052b86deaf5
nepp-plus <nepp.plus@gmail.com>
2021-12-27 12:02:54 +0900

    오버로딩 지원 시도 예시

--- cat.py
+++ cat.py
@@ -5,15 +5,15 @@ class Cat(Animal):
     # 동물 : 출생년도 / 성별  +  달리기(메쏘드)
     
     # 기능 추가 - 고양이만의 별도 기능.
-    def play_with_human(self, owner_name):
-        print(f'고양이가 {owner_name}과 놀아줍니다.')
+    def play_with_human(self, owner_name=None):
+        
+        if owner_name != None:
+            print(f'고양이가 {owner_name}과 놀아줍니다.')
+        else:
+            print('고양이가 사람과 놀아줍니다.')
         
     # 물려받은 기능 수정 - 고양이가 뛰어다니는 기능.
     def run(self):
         # 원래 동물로써 뛰어다니는 기능도 같이 실행.
         super().run()
         print('고양이가 뛰어다닙니다.')
-        
-    # 사람과 놀아주는 기능 추가 작성
-    def play_with_human(self):
-        print('고양이가 사람과 놀아주는 두번째 메쏘드.')
