5864fb81a361947bbe0d832d5d28a0a635440125
nepp-plus <nepp.plus@gmail.com>
2021-12-27 10:17:35 +0900

    함수 / 메쏘드의 비교

--- dog.py
+++ dog.py
@@ -4,3 +4,11 @@ class Dog:
         self.name = name
         self.birth_year = birth_year
         self.type = type
+        
+    
+    # 메쏘드 예시
+    def print_dog_info(self):
+        # 메쏘드 : 어떤 객체가 수행하는 그 객체의 기능.
+        # 어느 객체 (self 변수에 담아두자)가 수행하는지? 알아낸 상태로 코드를 작성해야함.
+        
+        print(f'강아지 이름 : {self.name}')
